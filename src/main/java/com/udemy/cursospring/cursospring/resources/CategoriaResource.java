df261c0bf0261d070a104a4258b3b6fae0b49531
Robson Lopes <rob_lopes7@hotmail.com>
2020-05-28 07:29:18 -0300

    Banco H2 e Categoria

--- src/main/java/com/udemy/cursospring/cursospring/resources/CategoriaResource.java
+++ src/main/java/com/udemy/cursospring/cursospring/resources/CategoriaResource.java
@@ -1,15 +1,26 @@
 package com.udemy.cursospring.cursospring.resources;
 
+import com.udemy.cursospring.cursospring.model.Categoria;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping("/categorias")
 public class CategoriaResource {
 
     @RequestMapping(method = RequestMethod.GET)
-    public String listar() {
-        return "Rest está funcioando!";
+    public List<Categoria> listar() {
+        Categoria categoria1 = new Categoria(1, "Informatica");
+        Categoria categoria2 = new Categoria(1, "Escritorio");
+
+        List<Categoria> categorias = new ArrayList<>();
+        categorias.add(categoria1);
+        categorias.add(categoria2);
+
+        return  categorias;
     }
 }
