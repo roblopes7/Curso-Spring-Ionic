33239a77166b6f84e065d5a2f165da87ec44d1bd
roblopes7 <42593177+roblopes7@users.noreply.github.com>
2020-06-07 09:32:20 -0300

    Personalizacao do retorno da validacao

--- src/main/java/com/udemy/cursospring/cursospring/resources/exceptions/ResourceExceptionHandler.java
+++ src/main/java/com/udemy/cursospring/cursospring/resources/exceptions/ResourceExceptionHandler.java
@@ -4,6 +4,8 @@ import com.udemy.cursospring.cursospring.services.exceptions.DataIntegrityExcept
 import com.udemy.cursospring.cursospring.services.exceptions.ObjectNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -23,6 +25,17 @@ public class ResourceExceptionHandler {
     public ResponseEntity<StandardError> dataIntegrity(DataIntegrityException e,
                                                         HttpServletRequest request){
         StandardError se = new StandardError(HttpStatus.BAD_REQUEST.value(), e.getMessage(), System.currentTimeMillis());
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(se);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<StandardError> methodArgumentNotValid(MethodArgumentNotValidException e,
+                                                       HttpServletRequest request){
+        ValidationError se = new ValidationError(HttpStatus.BAD_REQUEST.value(), "Erro de validação", System.currentTimeMillis());
+        for(FieldError fe : e.getBindingResult().getFieldErrors()){
+            se.addError(fe.getField(), fe.getDefaultMessage());
+        }
+
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(se);
     }
 }
