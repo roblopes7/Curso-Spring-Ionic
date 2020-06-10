3535ed5b4c8bc104224b745b5374ade20c6c6653
roblopes7 <42593177+roblopes7@users.noreply.github.com>
2020-06-10 07:13:36 -0300

    DELETE para Cliente com Endereço

--- src/main/java/com/udemy/cursospring/cursospring/model/Cliente.java
+++ src/main/java/com/udemy/cursospring/cursospring/model/Cliente.java
@@ -20,7 +20,7 @@ public class Cliente implements Serializable {
     private String email;
     private String cpfOuCnpj;
     private Integer tipoCliente;
-    @OneToMany(mappedBy = "cliente")
+    @OneToMany(mappedBy = "cliente", cascade = CascadeType.ALL)
     private List<Endereco> enderecos = new ArrayList<>();
     @ElementCollection
     @CollectionTable(name = "TELEFONE")
